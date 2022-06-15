2147683bd9fe8a70b5d137146c4c544a582dbe92
Josep <josepcortes6@gmail.com>
2022-06-15 23:28:31 +0200

    i1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const { Schema } = new Schema
+
+const droneModel ={
+    name:{
+        type: String,
+    },
+    propellers:{
+        type: Number,
+    },
+    maxSpeed:{
+        type: Number
+    }
+}
+
+const Drone = mongoose.model('Drone', droneModel)
+module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose')
+const Drone = require ('../models/Drone.model')
+
+const newDrones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+] 
+
+mongoose.connect()
+.then((x) => console.log (`Conected to ${x.collection.name}`))
+.then(() => {
+    mongoose.create.Drone(newDrones)
+})
+.catch((err) => {
+    console.log(err)
+})
+.finally(() => {
+    mongoose.connection.close()
+})
